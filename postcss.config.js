55bf7f709162e083f453c01d60130962f6090a29
Walkercito <yoanky2005@gmail.com>
2025-03-13 19:10:35 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {
-      tailwindConfig: "./tailwind.config.js" // Referencia explícita
-    },
+    tailwindcss: {},
     autoprefixer: {}
   }
 };
